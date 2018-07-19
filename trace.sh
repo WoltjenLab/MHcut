db6d51125b046055be2b8736f13244afd0f955ef
Jean Monlong <jean.monlong@mail.mcgill.ca>
2018-07-19 12:06:55 +0900

    dbsnp + clinvar

--- trace.sh
+++ trace.sh
@@ -48,19 +48,31 @@ python MHcut.py -var clinvar-grch38-all-deletion.tsv -ref hg38.fa -out clinvar-g
 
 ###
 ###
+##
+### All dbSNP
+curl ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh38p7/VCF/All_20180418.vcf.gz | gunzip -c | grep "VC=DIV" | awk '{if(length($4)>length($5)){print $0}}' | gzip > dbsnp-del.vcf.gz
+python prepareVcfIndel.py -vcf dbsnp-del.vcf.gz -o dbsnp-gene.tsv -info "RS|0,CAF|1,GENEINFO|0,PM|0" -infomerge 'MC|NSF,NSM,NSN,SYN,U3,U5,ASS,DSS,INT,R3,R5' -addchr
+gzip dbsnp-del.tsv
+
 ### All dbSNP aroung genes and ClinVar
 curl ftp://ftp.ncbi.nih.gov/snp/organisms/human_9606_b151_GRCh38p7/VCF/All_20180418.vcf.gz | gunzip -c | grep "VC=DIV"  | grep -E ";NSF;|;NSM;|;NSN;|;SYN;|;U3;|;U5;|;R3;|;R5;|;INT;|;ASS;|;RSS;|;REF;|;NSF$|;NSM$|;NSN$|;SYN$|;U3$|;U5$|;R3$|;R5$|;INT$|;ASS$|;RSS$|;REF$" | awk '{if(length($4)>length($5)){print $0}}' | gzip > dbsnp-gene-del.vcf.gz
-python prepareVcfIndel.py -vcf dbsnp-nsf-div-del.vcf -o dbsnp-nsf-div-del.tsv -info "RS|0,CAF|1,GENEINFO|0" -addchr
+python prepareVcfIndel.py -vcf dbsnp-gene-del.vcf.gz -o dbsnp-gene-del.tsv -info "RS|0,CAF|1,GENEINFO|0,PM|0" -addchr
+gzip dbsnp-gene-del.tsv
 
-wget -O variant_summary_10July2018.txt.gz ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/variant_summary.txt.gz
-zcat variant_summary_10July2018.txt.gz | awk 'BEGIN{FS="\t"; OFS="\t"; print "chr\tstart\tend\tRS\tGeneSymbol\tdbVar\tClinicalSignificance\treviewStatus\tnbSubmitters"}{if($17=="GRCh38" && $19!="na" && $2=="deletion"){print "chr"$19,$20,$21,$10,$5,$11,$7,$25,$26}}' | sort -u > clinvar-grch38-deletion.tsv
+wget ftp://ftp.ncbi.nih.gov/pub/clinvar/vcf_GRCh38/clinvar_20180701.vcf.gz
+python prepareVcfIndel.py -vcf clinvar_20180701.vcf.gz -o clinvar_20180701.tsv -info "AF_EXAC|0,AF_TGP|0,ALLELEID|0,CLNDN|-,CLNSIG|-,CLNVC|0,DBVARID|-,GENEINFO|1,MC|-,RS|0" -addchr
+wget -O var_citations_17July2018.txt ftp://ftp.ncbi.nlm.nih.gov/pub/clinvar/tab_delimited/var_citations.txt
+python prepareClinVar.py -tsv clinvar_20180701.tsv -cit var_citations_17July2018.txt -o clinvar-grch38-deletion_20180701.tsv
 
-# Merge the two TODO
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_28/gencode.v28.annotation.gtf.gz
+
+# Merge the ClinVar and dbSNP and annotate with Gencode
+Rscript mergeClinVarDbSNPGencode.R clinvar-grch38-deletion.tsv.gz dbsnp-gene-del.tsv gencode.v28.annotation.gtf.gz dbsnp-clinvar-deletion.tsv
 
 ## Run the faster version that uses JellyFish khmer counting
 jellyfish count -m 20 -s 100M -t 10 hg38.fa ## Prepare count table
 
-python MHcut.py -var dbsnp-div-del.tsv -ref hg38.fa -jf mer_counts.jf -out dbsnp-div-del
+python MHcut.py -var dbsnp-clinvar-deletion.tsv.gz -ref hg38.fa -jf mer_counts.jf -out dbsnp-clinvar-deletion
 
 ###
 ###
