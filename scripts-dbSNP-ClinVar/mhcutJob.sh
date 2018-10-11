2aa45b826f55d0c832a6464e5b062ab186b46445
jmonlong <jmonlong@ucsc.edu>
2018-10-11 14:59:55 -0700

    use existing chunk, inc. for parallel analysis

--- scripts-dbSNP-ClinVar/mhcutJob.sh
+++ scripts-dbSNP-ClinVar/mhcutJob.sh
@@ -6,14 +6,18 @@
 
 echo "Start:`date`"
 
-if [ $2 -ne "1" ]; then
-    head -1 $1.tsv > temp-mhcut-$1-$2.tsv
-    split -d -n l/$2/30 $1.tsv >> temp-mhcut-$1-$2.tsv
+if [ -s temp-mhcut-$1-$2.tsv ]; then
+    echo 'Using existing chunk file.'
 else
-    split -d -n l/$2/30 $1.tsv > temp-mhcut-$1-$2.tsv
+    if [ $2 -ne "1" ]; then
+	head -1 $1.tsv > temp-mhcut-$1-$2.tsv
+	split -d -n l/$2/30 $1.tsv >> temp-mhcut-$1-$2.tsv
+    else
+	split -d -n l/$2/30 $1.tsv > temp-mhcut-$1-$2.tsv
+    fi
 fi
 
-module load jmonlong/python mugqic/jellyfish/2.2.6
+module load mugqic/jellyfish/2.2.6
 python MHcut.py -var temp-mhcut-$1-$2.tsv -ref hg38.fa -jf mer_counts23.jf -out mhcut-$1-$2 -nofilt -minvarL 1
 
 echo "Finish:`date`"
